bb245c3dd084ed12aa8cd3fbdb43731979ef2db5
cristiansot <soluciongrafix@gmail.com>
2026-04-06 14:34:29 -0400

    init react vite bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">Apocalyptic</h1>
+      <button className="btn btn-primary mt-3">
+        Primer botón Bootstrap
+      </button>
+    </div>
+  );
+}
+
+export default App;
